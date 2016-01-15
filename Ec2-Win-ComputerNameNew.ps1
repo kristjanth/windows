881012a434562e75b181dabdc59112e264d66b85
Kristjan Thorvaldsson <kristjan@kt.is>
2016-01-15 11:28:02 +0000

    New script
    Due to change to names of instances in AWS from 8 to 17 char (part
    after "i-")

---
+++ Ec2-Win-ComputerNameNew.ps1
@@ -0,0 +1,6 @@
+$ComputerName = invoke-restmethod -uri http://169.254.169.254/latest/meta-data/instance-id
+$ComputerName -replace ".*-" 
+Set-ItemProperty -path "HKLM:\SYSTEM\CurrentControlSet\Control\Computername\Computername" -name "Computername" -value $ComputerName
+Set-ItemProperty -path "HKLM:\SYSTEM\CurrentControlSet\Control\Computername\ActiveComputername" -name "Computername" -value $ComputerName
+Set-ItemProperty -path "HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" -name "Hostname" -value $ComputerName
+Set-ItemProperty -path "HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" -name "NV Hostname" -value  $ComputerName
