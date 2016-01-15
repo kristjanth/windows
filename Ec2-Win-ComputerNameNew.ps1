2c399f33f42093657bfcf9cfc9105eb9567231d3
Kristjan Thorvaldsson <kristjan@kt.is>
2016-01-15 12:18:02 +0000

    Error fixing

--- Ec2-Win-ComputerNameNew.ps1
+++ Ec2-Win-ComputerNameNew.ps1
@@ -1,5 +1,5 @@
 $ComputerName = invoke-restmethod -uri http://169.254.169.254/latest/meta-data/instance-id
-$ComputerName -replace ".*-" 
+$ComputerName = $ComputerName -replace ".*-" 
 Set-ItemProperty -path "HKLM:\SYSTEM\CurrentControlSet\Control\Computername\Computername" -name "Computername" -value $ComputerName
 Set-ItemProperty -path "HKLM:\SYSTEM\CurrentControlSet\Control\Computername\ActiveComputername" -name "Computername" -value $ComputerName
 Set-ItemProperty -path "HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" -name "Hostname" -value $ComputerName
